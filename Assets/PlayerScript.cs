f0f702325fc2ec7088000cd0d0f4ad2959f87c5f
BoykoTV <tanya.titanya@gmail.com>
2020-04-21 00:48:16 +0300

    1

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    public float titl;
+    public float xMin, xMax, zMin, zMax;
+    Rigidbody playerShip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerShip = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        playerShip.velocity = new Vector3(moveHorizontal, 0, moveVertical) * speed;
+
+        float xPosition = Mathf.Clamp(playerShip.position.x, xMin, xMax);
+        float zPosition = Mathf.Clamp(playerShip.position.z, zMin, zMax);
+
+        playerShip.position = new Vector3(xPosition, 0, zPosition);
+
+        playerShip.rotation = Quaternion.Euler(playerShip.position.z * titl, 0, playerShip.position.x * titl);
+    }
+}
