fb41075f1e38ff463cc36db0eee1453b42c97415
BoykoTV <tanya.titanya@gmail.com>
2020-04-22 01:02:03 +0300

    add light, lazershots, asteroids, asteroid emitter, explosions, boundary,  deleting gone elements

---
+++ Assets/BoundaryScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryScript : MonoBehaviour
+{
+    //при выходе за границы текущего объекта
+    private void OnTriggerExit(Collider other) 
+    {
+        Destroy(other.gameObject);
+    }
+}
